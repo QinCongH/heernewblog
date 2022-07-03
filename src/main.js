b1d1be2decb50f6234ecb6f2e7ff04424ce19d0f
QinCongH <3409575546@qq.com>
2022-07-03 22:10:37 +0800

    首页图标点击复制图标提示文本

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import './common/css/style.css'
 import './common/css/layout.css'
 import './common/css/index.css'
 import less from 'less'
+import Clipboard from 'v-clipboard'
 import App from './App.vue'
 import router from './route/index'
-createApp(App).use(router,less).mount('#app')
+createApp(App).use(router,less,Clipboard).mount('#app')
