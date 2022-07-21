4664b5023a42add00c3cd4687be6a088e4c4bba3
QinCongH <3409575546@qq.com>
2022-07-21 22:46:01 +0800

    笔记本样式

--- src/pages/Notepad/Notepad.vue
+++ src/pages/Notepad/Notepad.vue
@@ -5,14 +5,14 @@
         <div class="content">
           <el-row :gutter="20">
             <el-col :md="6" :sm="12" :xs="24" v-for="v in 5" :key="v">
-              <div  class="notepad-content w-100 mg-t-15">
+              <div  class="notepad-content w-100 mg-t-30">
                 <img
                   ref="imgWidth"
                   class="max-w-100 border-10 block"
                   src="https://images.unsplash.com/photo-1657403538510-1b324c4c1c8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=762&q=80"
                   alt=""
                 />
-                <p class="text-center">
+                <p class="text-center mg-t-10">
                   javascript
                 </p>
               </div>
